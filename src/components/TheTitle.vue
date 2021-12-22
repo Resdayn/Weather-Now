a973c29c0edf63dbea8ce29b69c47b8a65c5a576
Juan Gonzalez <jgonzalez@naver.com>
2021-12-22 11:43:43 +0900

    restyled

--- src/components/TheTitle.vue
+++ src/components/TheTitle.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
-    <h1>Weather App</h1>
+    <h1>Weather Now</h1>
   </div>
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Audiowide&display=swap');
 h1 {
+  font-family: 'Audiowide', cursive;
   display: flex;
   flex-direction: column;
   place-items: center;
   justify-content: center;
-  font-size: 48px;
+  font-size: 60px;
   padding: 2rem 0;
-  color: rgb(240, 231, 231);
+  color: #FCFEFC;
+}
+
+@media screen and (max-width: 480px) {
+  h1 {
+    font-size: 40px;
+  }
 }
 </style>
